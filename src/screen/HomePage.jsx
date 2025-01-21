590ea4c594ce5f6cc509f00edad1c0cbef2bf206
mitul88 <shahed.rahman88@gmail.com>
2025-01-21 13:04:48 +0600

    background img added

--- src/screen/HomePage.jsx
+++ src/screen/HomePage.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const HomePage = () => {
-  return <div className="min-h-[calc(100vh-5rem)]">HomePage</div>;
+  return (
+    <div className="min-h-[calc(100vh-5rem)]">
+      <div
+        className="w-full h-80 bg-no-repeat bg-cover bg-center"
+        style={{ backgroundImage: 'url("/images/food-home-img.jpg")' }}
+      ></div>
+    </div>
+  );
 };
 
 export default HomePage;
