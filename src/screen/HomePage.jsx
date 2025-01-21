4d2dede8b59f7565e87ed44ac0da45e7bd030607
mitul88 <shahed.rahman88@gmail.com>
2025-01-21 13:29:07 +0600

    overlay, middle title div added

--- src/screen/HomePage.jsx
+++ src/screen/HomePage.jsx
@@ -4,9 +4,27 @@ const HomePage = () => {
   return (
     <div className="min-h-[calc(100vh-5rem)]">
       <div
-        className="w-full h-80 bg-no-repeat bg-cover bg-center"
+        className="w-full h-80 bg-no-repeat bg-cover bg-center relative"
         style={{ backgroundImage: 'url("/images/food-home-img.jpg")' }}
-      ></div>
+      >
+        <div className="z-10 flex flex-col gap-5 min-w-56 bg-gray-800 rounded-xl p-3 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          <h3 className="text-3xl text-white text-center font-bold uppercase">
+            Delicious food at your doorstep
+          </h3>
+          <div>
+            <p className="text-sm text-white text-center max-w-96 mx-auto italic mb-2">
+              Choose your favorite meal from our broad selection of available
+              meals and enjoy a delicious lunch or dinner at home.
+            </p>
+            <p className="text-sm text-white text-center max-w-96 mx-auto italic">
+              All our meals are cooked with high-quality ingredients,
+              just-in-time and of course by experienced chefs!
+            </p>
+          </div>
+        </div>
+
+        <div className="absolute top-0 bottom-0 left-0 w-full p-5 bg-white bg-opacity-30"></div>
+      </div>
     </div>
   );
 };
